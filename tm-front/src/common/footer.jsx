b646433e67c866c000b32ed63a7a5eabb57fe226
jungenjin <jej1217s1217@gmail.com>
2023-02-23 17:47:44 +0900

    jej 파일 정리

--- tm-front/src/common/footer.jsx
+++ tm-front/src/common/footer.jsx
@@ -8,17 +8,17 @@ function Footer() {
     <Container fluid="md">
     <div className="Footer">
       <footer className="App-Footer">
-      <footer>
-      <div className="container">
-      <div className="FtextBox">
-        <p className="Ftext">오시는 길</p>
-        <p className="Ftext">이용약관</p>
-        <p className="Ftext">개인정보처리방침</p>
-      </div>
-      <p className="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
-    </div>
-    </footer>
-    </footer>
+        <footer>
+          <div className="container">
+            <div className="FtextBox">
+              <p className="Ftext">오시는 길</p>
+              <p className="Ftext">이용약관</p>
+              <p className="Ftext">개인정보처리방침</p>
+            </div>
+            <p className="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
+          </div>
+        </footer>
+      </footer>
     </div>
     </Container>
   );
