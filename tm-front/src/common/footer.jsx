3ebd765e2fb4ceab466c46af5492fcecd26c6736
jungenjin <jej1217s1217@gmail.com>
2023-02-23 14:49:14 +0900

    jej min 콘솔 오류 수정

--- tm-front/src/common/footer.jsx
+++ tm-front/src/common/footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
     <div className="Footer">
       <footer className="App-Footer">
       <footer>
-      <div class="container">
-      <div class="FtextBox">
-        <p class="Ftext">오시는 길</p>
-        <p class="Ftext">이용약관</p>
-        <p class="Ftext">개인정보처리방침</p>
+      <div className="container">
+      <div className="FtextBox">
+        <p className="Ftext">오시는 길</p>
+        <p className="Ftext">이용약관</p>
+        <p className="Ftext">개인정보처리방침</p>
       </div>
-      <p class="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
+      <p className="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
     </div>
     </footer>
     </footer>
--- tm-front/src/pages/main.jsx
+++ tm-front/src/pages/main.jsx
@@ -5,27 +5,27 @@ function Main() {
   return (
     <div className="Main">
       <section className="App-Main">
-      <section class="main">
-        <div class="container">
-          <p class="mainText1">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
+      <section className="main">
+        <div className="container">
+          <p className="mainText1">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
             radiant boby  come to life” - by Tamara Shopsin</p>
-            <div class="mainText2">Color</div>
-            <div class="mainText3">is life</div>
-            <button class="mainBtn"><a class="mainTextBtn" href='/now'>전시보러가기</a></button>
+            <div className="mainText2">Color</div>
+            <div className="mainText3">is life</div>
+            <button className="mainBtn"><a className="mainTextBtn" href='/now'>전시보러가기</a></button>
         </div>
       </section>
-      <section class="content">
-          <div class="container">
-            <h2 class="h2">Preview</h2>
-            <div class="Pimg1"></div>
-            <div class="Pimg2"></div>
-            <div class="Pimg3"></div>
-            <div class="Pimg4"></div>
-            <div class="Pimg5"></div>
-            <div class="Pimg6"></div>
-            <div class="Pimg7"></div>
-            <div class="Pimg8"></div>
-            <div class="Pimg9"></div>
+      <section className="content">
+          <div className="container">
+            <h2 className="h2">Preview</h2>
+            <div className="Pimg1"></div>
+            <div className="Pimg2"></div>
+            <div className="Pimg3"></div>
+            <div className="Pimg4"></div>
+            <div className="Pimg5"></div>
+            <div className="Pimg6"></div>
+            <div className="Pimg7"></div>
+            <div className="Pimg8"></div>
+            <div className="Pimg9"></div>
           </div>
       </section>
     </section>
